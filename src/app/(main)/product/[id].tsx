2b83d07c7a33f78c763ef53de3bcd54c2a45a96e
fahryakbarik15 <akbarfahry1506@gmail.com>
2026-07-28 13:27:31 +0700

    SIKIT-LAGI-HOMENYA

--- src/app/(main)/product/[id].tsx
+++
@@ -1,1 +0,0 @@
-import { View, Text } from 'react-native'; export default function ProductScreen() { return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Product Screen</Text></View>; }
